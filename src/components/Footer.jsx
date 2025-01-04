75d144b2467f7e4c8b3b0f5d0a202ac8e624cbe2
Narupo <pouzumaki@gmail.com>
2025-01-04 16:42:16 -0600

    set up basic functionality and routing for the whole portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 My Name</p>
+      <ul>
+        <li>
+          <a href="https://github.com/my-username" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/my-profile" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/my-profile" target="_blank" rel="noopener noreferrer">
+            Twitter
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
